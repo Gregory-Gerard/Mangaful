30efff8824356cc7ad2c4e1cc3e89145bca94ecb
Grégory GERARD <contact@gregory-gerard.dev>
2020-05-17 01:00:25 +0200

    [ADD] API Manga endpoints

--- front/constants/api.js
+++ front/constants/api.js
@@ -1,5 +1,8 @@
 export const API_ROOT = () => 'http://localhost/perso/mangaful/api/public/api'
 
+export const MANGAS_ROOT = () => `${API_ROOT()}/mangas`
+export const GET_MANGA = mangaId => `${API_ROOT()}/mangas/${mangaId}`
+
 export const CHAPTERS_ROOT = () => `${API_ROOT()}/chapters`
 export const LAST_CHAPTERS = () => `${CHAPTERS_ROOT()}/last`
 
