1f981e1eae809ec52e16bbdb087cf207810276a2
Grégory GERARD <contact@gregory-gerard.dev>
2020-05-17 12:55:06 +0200

    [ADD] Composant couverture (similaire à Manga.vue mais sans les infos, juste une image)

---
+++ front/components/Cover.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-lazy:background-image="image" :class="$style.cover" />
+</template>
+
+<script>
+export default {
+  name: 'Cover',
+  props: {
+    image: {
+      type: String,
+      default: '',
+      required: true
+    }
+  }
+}
+</script>
+
+<style module lang="scss">
+  .cover {
+    position: relative;
+    border-radius: $border-radius;
+    padding-bottom: (5/3.33) * 100%;
+    background-color: $gray-800;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
